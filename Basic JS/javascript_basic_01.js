63f4b001087d4623fc69db376f4f6ba8d2a089fc
Enzo Godoy <daios.inc@gmail.com>
2020-09-15 20:27:33 +0200

    Cleaned 10 folders 10_19

---
+++ Basic JS/javascript_basic_01.js
@@ -0,0 +1,36 @@
+
+//Especial characters 
+var myEscapeChar;
+
+myEscapeChar = "We\'d like to mention:\t For the first price \n"; 
+myEscapeChar = myEscapeChar + "\\ The winner it\'s Mr.N";
+
+//Concatening strings : 
+var firstStr  = "Who is" + "The king on Ocliuba's Islands"; 
+var secondStr = "Mr.Chicke says" + "The sky is falling"; 
+var myEnd= "Were all of this begin " + "The end will appear";
+
+
+//Concatening with shortcut 
+var someQuote = "In the mountains" ; 
+var someQuote2 = "Some creatures exists" ; 
+
+someQuote += someQuote2 ; 
+ 
+ // Determining size of a string by functions 
+ var lengthQuote = 0 ; 
+ var myQuote = "If you saw the end, why aren't you avoiding it"; 
+
+ lengthQuote = myQuote.length; 
+ 
+ //Acessing characters of a string ; 
+var someQuote = "Atlas was the father of zeus"; 
+var fatherZeus = someQuote[1];
+fatherZeus +=  someQuote[2];
+fatherZeus +=  someQuote[3];
+fatherZeus +=  someQuote[4];
+fatherZeus +=  someQuote[5];
+
+console.log("The first letter of the Father of Zeus it was : " + fatherZeus.length[1]); 
+
+ 
