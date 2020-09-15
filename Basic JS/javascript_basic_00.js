3ead5098bea894cc3fecd1956ac9cbcbe8482310
Enzo Godoy <daios.inc@gmail.com>
2020-09-15 20:10:42 +0200

    Cleaned 10 folders 0_1

---
+++ Basic JS/javascript_basic_00.js
@@ -0,0 +1,70 @@
+//Samples of definition vars : 
+var someVar1; 
+var someVar2; 
+
+ //initialization of vars 
+ var quantityCars = 4; 
+ var factorNumber = 11;
+ var someName = "Arthur"; 
+
+ // Assignation sample 0_1 
+ var firstNumber = 3 ; 
+ var secondNumber = 22 ; 
+ var lastName = "Rimbaud"; 
+
+ //Assignations sample_02
+ var firstNumb  = 23 ; 
+ var secondNumb = 24; 
+ var company = "Michael Scott"
+
+//Assignation and operations : 
+firstNumb  = firstNumb + 3 ; 
+secondNumb = secondNumb + 5;
+company = company + "Paper Company"; 
+
+//Declarations 
+var studlyCapVar;
+var properCamelCase; 
+var titleCaseOVer; 
+
+studlyCapvar    = 13 ; 
+properCamelCase = 25 ; 
+titleCaseOver   = 544 ;
+var someRandomNum = 53 ; 
+
+ // Increment 
+ properCamelCase++; 
+
+ //Decrement 
+ properCamelCase--; 
+
+ #Operations with modifiers increment 
+ var someNum1 = 34 ; 
+ var someNum2 = 43 ; 
+ var someNum3 = 45 ; 
+
+ someNum1 += 6; 
+ someNum2 += 7; 
+ someNum3 += 5;
+
+
+ #Operations with modifiers decrement ; 
+ var someNum1 = 21 ; 
+ var someNum2 = 32 ; 
+ var someNum3 = 24 ; 
+
+ someNum1 -= 9; 
+ someNum2 -= 8; 
+ someNum3 -= 6;
+
+//sub-assigned string 
+var firstName = "Michael Scott"; 
+var lastName  = "Simon"; 
+
+var firstEmployee  = firstName; 
+var secondEmployee = lastName; 
+ 
+ //Scaped string 
+ var someString = "I have a \"double mentioned\" phrase somewhere .";
+ 
+
